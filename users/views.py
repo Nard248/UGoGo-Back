afe93626bbf89936ad10bfef816e10948c42ad19
simon.galstyan1 <simon.galstyan@puzl.ai>
2025-02-26 02:28:36 +0400

    - small fix

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.db.utils import UserEmailVerificationService
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
